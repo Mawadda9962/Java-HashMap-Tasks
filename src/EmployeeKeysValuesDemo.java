ec86d873992bb1b32665d12517e40fb25c9617a1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-14 17:10:22 +0400

    Solving Task8 -Display Keys and Values from a Java HashMap

---
+++ src/EmployeeKeysValuesDemo.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeKeysValuesDemo {
+    public static void main(String[] args){
+        Map<Integer, String> employeeRecords = new HashMap<>();
+
+        // Adding employee records using put() method
+        employeeRecords.put(111, "Ahmed");
+        employeeRecords.put(222, "Salim");
+        employeeRecords.put(333, "Nasser");
+        employeeRecords.put(444, "Sara");
+        employeeRecords.put(555, "Waleed");
+
+        //Displaying all elements in the HashMap using for each
+        System.out.println("Employee Records:");
+        for(Map.Entry<Integer, String> n :employeeRecords.entrySet()){
+            System.out.println("ID:" + n.getKey() + " " + "names:" +  n.getValue());
+        }
+
+        //Displaying all employee IDs
+        System.out.println(employeeRecords.keySet());
+
+        //Displaying all employee names
+        System.out.println(employeeRecords.values());
+
+        //Adding one more employee record
+        employeeRecords.put(888, "Kalid");
+
+        //Displaying all elements in the HashMap using for each
+        System.out.println("Employee Records:");
+        for(Map.Entry<Integer, String> n :employeeRecords.entrySet()){
+            System.out.println("ID:" + n.getKey() + " " + "names:" +  n.getValue());
+        }
+    }
+}
