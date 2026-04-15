a73226cec873122f8645b79338acfbe910d431a1
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-15 10:09:12 +0400

    Splving Task19 Part1

---
+++ src/EmployeeMapDemo.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class EmployeeMapDemo {
+    public static void main(String[] args){
+        Map<Integer, String> employeeRecords = new HashMap<>();
+
+        // Adding employee records using put() method
+        employeeRecords.put(111, "Ahmed");
+        employeeRecords.put(222, "Salim");
+        employeeRecords.put(333, "Nasser");
+        employeeRecords.put(444, "Sara");
+        employeeRecords.put(555, "Waleed");
+
+        //Displaying all elements in the HashMap using for each
+        System.out.println("Employee Records:");
+        for(Map.Entry<Integer, String> n :employeeRecords.entrySet()){
+            System.out.println("ID:" + n.getKey() + " " + "names:" +  n.getValue());
+        }
+
+    }
+}
